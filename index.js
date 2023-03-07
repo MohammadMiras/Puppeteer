8a6601cdc1bd3c3ec7445881080acbcdddc8d59b
mohammadmiras <mohammad.miras.1377@gmail.com>
2023-03-07 14:42:16 +0330

    k

--- index.js
+++ index.js
@@ -11,26 +11,19 @@ const puppeteer = require("puppeteer");
         );
     const page = await browser.newPage();
 
-    await page.goto('https://web.telegram.org/k/#@v2rayngvpn1n');
+    await page.goto('https://web.telegram.org/k/#@qheimat_Sekeh');
 
-     await page.setViewport({ width: 1080, height: 1024 });
-
-    // await page.waitForSelector('p');   
-
-    
-
-    // // const fullTitle = await textSelector.evaluate(el => el.textContent);
-    // // console.log(fullTitle);
-    // // // Print the full title
-    // // console.log('The title of this blog post is "%s".', fullTitle);
-
+    // await page.waitForSelector('p');  
     setTimeout(async () => {
 
-        // await page.screenshot({ path: 'example.png' });
-        const texts = await page.evaluate( () => Array.from( document.querySelectorAll( '.message' ), element => element.textContent) );
+        setInterval(async ()=>{
+            const texts = await page.evaluate( () => document.querySelectorAll(".message")[document.querySelectorAll(".message").length - 1].textContent);
+            console.log("====================");
+            console.log(texts);
+            console.log("====================");
+    
+        }, 5000);
 
-     console.log(texts);
-        await browser.close();
-    }, 120_000)
+    }, 60_000)  
    
 })();
